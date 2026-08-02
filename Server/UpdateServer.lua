91511316d1a04a8c10e0e2d924934c92dab3c731
4ank Vylson <vanastanas55@gmail.com>
2026-08-03 02:19:40 +0300

    Update UpdateServer.lua

--- Server/UpdateServer.lua
+++ Server/UpdateServer.lua
@@ -1,1 +1,35 @@
-...
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local UpdateConfig = require(ReplicatedStorage.Modules.UpdateConfig)
+
+local updateEventFolder = ReplicatedStorage:WaitForChild("UpdateEvent")
+local updateViewedEvent = updateEventFolder:WaitForChild("UpdateViewedEvent")
+
+updateViewedEvent.OnServerEvent:Connect(function(player, viewedUpdate)
+	if player:GetAttribute("DataReady") ~= true then
+		return
+	end
+	
+	if viewedUpdate ~= UpdateConfig.CurrentUpdate then
+		warn("Player tried to mark invalid update as viewed:", player.Name, viewedUpdate)
+		return
+	end
+	
+	local playerData = player:FindFirstChild("PlayerData")
+	if not playerData then
+		return
+	end
+	
+	local lastSeenUpdate = playerData:FindFirstChild("LastSeenUpdate")
+	if not lastSeenUpdate or not lastSeenUpdate:IsA("StringValue") then
+		return
+	end
+	
+	if lastSeenUpdate.Value == viewedUpdate then
+		return
+	end
+	
+	lastSeenUpdate.Value = viewedUpdate
+	
+	print("UPDATE VIEWED:", player.Name, viewedUpdate)
+end)
