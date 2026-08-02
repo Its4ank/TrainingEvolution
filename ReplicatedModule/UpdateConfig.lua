9f1bf5cac91c7c405cf7191ba73d86527eba7d97
4ank Vylson <vanastanas55@gmail.com>
2026-08-03 02:31:50 +0300

    Update version and button names in UpdateConfig

--- ReplicatedModule/UpdateConfig.lua
+++ ReplicatedModule/UpdateConfig.lua
@@ -1,20 +1,20 @@
 local UpdateConfig = {}
 
-UpdateConfig.CurrentUpdate = "1.0"
+UpdateConfig.CurrentUpdate = "1.1"
 
 UpdateConfig.Updates = {
 	["1.0"] = {
-		ButtonName = "UPDButton",
+		ButtonName = "UPD0Button",
 		FrameName = "ScrollingUPD0",
 	},
 	
 	["1.1"] = {
-		ButtonName = "UPDButton1",
+		ButtonName = "UPD1Button",
 		FrameName = "ScrollingUPD1",
 	},
 	
 	["1.2"] = {
-		ButtonName = "UPDButton2",
+		ButtonName = "UPD2Button",
 		FrameName = "ScrollingUPD2",
 	},
 }
