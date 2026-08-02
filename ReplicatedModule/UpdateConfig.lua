e75e12200afa7815dda54fd2fa4de185a8233bad
4ank Vylson <vanastanas55@gmail.com>
2026-08-03 02:21:05 +0300

    Update UpdateConfig.lua

--- ReplicatedModule/UpdateConfig.lua
+++ ReplicatedModule/UpdateConfig.lua
@@ -1,1 +1,22 @@
-...
+local UpdateConfig = {}
+
+UpdateConfig.CurrentUpdate = "1.0"
+
+UpdateConfig.Updates = {
+	["1.0"] = {
+		ButtonName = "UPDButton",
+		FrameName = "ScrollingUPD0",
+	},
+	
+	["1.1"] = {
+		ButtonName = "UPDButton1",
+		FrameName = "ScrollingUPD1",
+	},
+	
+	["1.2"] = {
+		ButtonName = "UPDButton2",
+		FrameName = "ScrollingUPD2",
+	},
+}
+
+return UpdateConfig
