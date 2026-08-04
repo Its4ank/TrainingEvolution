f7388a3d59e408036ff3ceb463d4cad9d6367fa4
4ank Vylson <vanastanas55@gmail.com>
2026-08-04 23:59:41 +0300

    Update EnergyPopup.lua

--- UI/EnergyPopup.lua
+++ UI/EnergyPopup.lua
@@ -1,34 +1,55 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
 
 local energyPopupEvent = ReplicatedStorage:WaitForChild("EnergyPopupEvent")
 
-local popupTemplate = script.Parent:WaitForChild("EnergyGainFrame"):WaitForChild("EnergyGainLabel")
+local raceGui = script.Parent
+
+local popupTemplate = raceGui:WaitForChild("EnergyGainFrame"):WaitForChild("EnergyGainLabel")
+
+local function formatNumber(number)
+	number = tonumber(number) or 0
+	
+	local suffixes = {
+		{Value = 1e30, Suffix = "N"}, -- Нонилион
+		{Value = 1e27, Suffix = "O"}, --Октиллион
+		{Value = 1e24, Suffix = "S"}, --Септиллион
+		{Value = 1e21, Suffix = "Sp"}, --Секстиллион
+		{Value = 1e18, Suffix = "Qd"}, --Квинтиллион
+		{Value = 1e15, Suffix = "Q"}, --Квадриллион
+		{Value = 1e12, Suffix = "T"}, --Триллион
+		{Value = 1e9, Suffix = "B"}, --Миллиард
+		{Value = 1e6, Suffix = "M"}, --Миллион
+		{Value = 1e3, Suffix = "K"}, --Тысяча
+	}
+	
+	for _, data in ipairs(suffixes) do
+		if number >= data.Value then
+			local short = number / data.Value
+
+			if short >= 100 then
+				return string.format("%.0f%s", short, data.Suffix)
+			elseif short >= 10 then
+				return string.format("%.1f%s", short, data.Suffix)
+			else
+				return string.format("%.2f%s", short, data.Suffix)
+			end
+		end
+	end
+	return tostring(math.floor(number))
+end
 
 energyPopupEvent.OnClientEvent:Connect(function(amount)
 	local popup = popupTemplate:Clone()
 
-	local displayText
-
-	if amount % 1 == 0 then
-		displayText = "+" .. tostring(math.floor(amount))
-	else
-		displayText = string.format("+%.1f", amount)
-	end
-
-	popup.Text = displayText
+	popup.Text = "+" .. formatNumber(amount)
 	popup.Visible = true
+	popup.TextTransparency = 0
 	popup.Parent = popupTemplate.Parent
 
-	popup.Position = UDim2.new(
-		0.5,
-		math.random(-50,50),
-		0.5,
-		math.random(-20,20)
-	)
+	popup.Position = UDim2.new(0.5, math.random(-50,50), 0.5, math.random(-20,20))
 
-	local tween = game:GetService("TweenService"):Create(
-		popup,
-		TweenInfo.new(1),
+	local tween =TweenService:Create(popup, TweenInfo.new(1),
 		{
 			Position = popup.Position - UDim2.new(0,0,0.1,0),
 			TextTransparency = 1,
