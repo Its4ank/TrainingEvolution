42bbbafcc2e7c8d3f4eab5e40336317622aa6d45
4ank Vylson <vanastanas55@gmail.com>
2026-09-21 00:34:31 +0300

    Refactor stats formatting to use FormatModule

--- UI/StatsUI.lua
+++ UI/StatsUI.lua
@@ -4,10 +4,11 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local ClientDataModule = require(game.ReplicatedStorage.Modules.ClientDataModule)
+local FormatModule = require(game.ReplicatedStorage.Modules.FormatModule)
 
 local raceGui = script.Parent
 local player = Players.LocalPlayer
-ClientDataModule.WaitUntilReade(player)
+ClientDataModule.WaitUntilReady(player)
 
 local guiFolder = raceGui:WaitForChild("GuiFolder")
 
@@ -27,29 +28,11 @@ local moneyLabel = uiValueFrame:WaitForChild("MoneyLabel")
 local gemsLabel = uiValueFrame:WaitForChild("GemsLabel")
 local rebirthLabel = uiValueFrame:WaitForChild("RebirthLabel")
 
-local function formatShort(n)
-	if n >= 1e18 then
-		return string.format("%.1fQ", n / 1e18)
-	elseif n >= 1e15 then
-		return string.format("%.1fQa", n / 1e15)
-	elseif n >= 1e12 then
-		return string.format("%.1fT", n / 1e12)
-	elseif n >= 1e9 then
-		return string.format("%.1fB", n / 1e9)
-	elseif n >= 1e6 then
-		return string.format("%.1fM", n / 1e6)
-	elseif n >= 1e3 then
-		return string.format("%.1fK", n / 1e3)
-	else
-		return tostring(math.floor(n))
-	end
-end
-
 local function updateStatsUI()
-	energyLabel.Text = "" .. formatShort(energyStat.Value)
-	moneyLabel.Text = "" .. formatShort(moneyStat.Value)
-	gemsLabel.Text = "" .. formatShort(gemsStat.Value)
-	rebirthLabel.Text = "" .. formatShort(rebirthStat.Value)
+	energyLabel.Text = "" .. FormatModule.FormatShort(energyStat.Value)
+	moneyLabel.Text = "" .. FormatModule.FormatShort(moneyStat.Value)
+	gemsLabel.Text = "" .. FormatModule.FormatShort(gemsStat.Value)
+	rebirthLabel.Text = "" .. FormatModule.FormatShort(rebirthStat.Value)
 end
 
 energyStat.Changed:Connect(updateStatsUI)
